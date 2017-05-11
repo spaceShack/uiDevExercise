34c2bfdc7d22b5e1adfa763d79bab2dac7a398b0
Andre Camacho <andre.camacho.a@gmail.com>
2017-05-11 00:49:10 -0400

    Added support to call holidays api. Added error control, and ui rendering of holidays in the calendar.

--- js/script.js
+++ js/script.js
@@ -7,10 +7,13 @@
             };
 
             $('#userInput').submit(function(e) {
+                var startDate = moment($('#inputStartDate').val(), 'MM/DD/YYYY');
+                var startingYear = startDate.format('YYYY');
                 var days = [];
                 e.preventDefault();
-                days = generateCalendarArray();
+                days = generateCalendarArray(startDate);
                 renderCalendar(days);
+                requestAndFillHolidays(startingYear);
             });
         }
 
@@ -20,14 +23,14 @@
             for(var i=0;i<days.length;i++) {
                 var newDay = $('<div/>', {
                     'class': days[i].classAttr,
-                    'text': days[i].text
+                    'text': days[i].text,
+                    'id': days[i].unixTime
                 });
                 calendar.append(newDay);
             }
         }
 
-        function generateCalendarArray() {
-            var startDate = moment($('#inputStartDate').val(), 'MM/DD/YYYY');
+        function generateCalendarArray(startDate) {
             var numberOfDays = $('#inputNumberDays').val();
             var days = [];
             var currentMonth = startDate.format('M');
@@ -51,20 +54,21 @@
 
         function generateLiteralDays() {
             return [
-                {text: 'Sun', classAttr: 'day title-day'},
-                {text: 'Mon', classAttr: 'day title-day'},
-                {text: 'Tue', classAttr: 'day title-day'},
-                {text: 'Wed', classAttr: 'day title-day'},
-                {text: 'Thu', classAttr: 'day title-day'},
-                {text: 'Fri', classAttr: 'day title-day'},
-                {text: 'Sat', classAttr: 'day title-day'}
+                {text: 'Sun', classAttr: 'day title-day', unixTime: 0},
+                {text: 'Mon', classAttr: 'day title-day', unixTime: 0},
+                {text: 'Tue', classAttr: 'day title-day', unixTime: 0},
+                {text: 'Wed', classAttr: 'day title-day', unixTime: 0},
+                {text: 'Thu', classAttr: 'day title-day', unixTime: 0},
+                {text: 'Fri', classAttr: 'day title-day', unixTime: 0},
+                {text: 'Sat', classAttr: 'day title-day', unixTime: 0}
             ];
         }
 
         function generateMonthInformation(momentDate) {
             var result = {
                 text: momentDate.format('MMMM YYYY'),
-                classAttr: 'month-title'
+                classAttr: 'month-title',
+                unixTime: 0
             }
             return result;
         }
@@ -75,7 +79,8 @@
                 for(var i=0;i<count;i++) {
                     result.push({
                         text: '',
-                        classAttr: 'day empty-day'
+                        classAttr: 'day empty-day',
+                        unixTime: 0
                     });
                 }
             }
@@ -85,9 +90,64 @@
         function generateNormalDay(momentDate) {
             var result = {
                 text: momentDate.format('D'),
-                classAttr: 'day ' + (momentDate.format('E') == 6 || momentDate.format('E') == 7 ? 'weekend-day' : 'normal-day')
+                classAttr: 'day ' + (momentDate.format('E') == 6 || momentDate.format('E') == 7 ? 'weekend-day' : 'normal-day'),
+                unixTime: momentDate.format('x')
             };
             return result;
+        }
+
+        function requestAndFillHolidays(startYear) {
+            function processHolidaysSuccess(response) {
+                var holidays = [];
+                switch (response.status) {
+                    case 200:
+                        holidays = response.holidays;
+                        for (holiday in holidays) {
+                            var unixHoliday = moment(holiday, 'YYYY-MM-DD').format('x');
+                            var elementExists = $('#'+unixHoliday);
+                            if(elementExists.length > 0) {
+                                elementExists.attr('class','day holiday-day');
+                            }
+                        }
+                        break;
+                    default:
+                        console.log(response.error);
+                        showError(response.error);
+                }
+            }
+            function processHolidayserror(error, response) {
+                //success
+                console.log(error);
+                showError('There was an error with the request');
+            }
+
+            function getSerializedData() {
+                var queryData = 'key=de2833a0-c033-43c5-b8cb-82c78edc714b&country=' + $('#inputCountryCode').val() + '&year=2008';
+                return queryData;
+            }
+
+            if (startYear == 2008) {
+                var serializedFormData = getSerializedData();
+                $.ajax({
+                    method: 'GET',
+                    url: 'https://holidayapi.com/v1/holidays?' + serializedFormData
+                })
+                .done(processHolidaysSuccess)
+                .fail(processHolidayserror);
+            }
+        }
+
+
+        function showError(text) {
+            $('.error-form .messageText').first().html(text);
+            $('.error-form').show('fast', function(){
+                setTimeout(
+                    function(){
+                        $('.error-form').hide('slow');
+                    },
+                    3000
+                );
+            });
         }
 
         onInit();
